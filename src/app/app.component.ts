b646c561528ace59b6d2e04e967f4e8c267fcb9c
Omprakash Kumar <omprakashkumar8404@gmail.com>
2022-05-13 10:28:12 +0530

    routing fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'lawnetics';
+export class AppComponent implements OnInit {
+  title ='lawnetics';
+	constructor(private router: Router) { }
+	
+	ngOnInit() {
+		this.router.events.subscribe((event) => {
+			if (!(event instanceof NavigationEnd)) {
+				return;
+			}
+			window.scrollTo(0, 0)
+		});
+	}
 }
