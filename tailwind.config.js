cdf776e7c1e31e4187e2075878f03ddef1fac1b9
Omprakash Kumar <omprakashkumar8404@gmail.com>
2022-05-14 17:56:34 +0530

    animation fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      animation: {
+        "kenburns-bottom": "kenburns-bottom 5s ease   both"
+    },
+    keyframes: {
+      "kenburns-bottom": {
+          "0%": {
+              transform: "scale(1) translateY(0)",
+              "transform-origin": "50% 84%"
+          },
+          to: {
+              transform: "scale(1.25) translateY(15px)",
+              "transform-origin": "bottom"
+          }
+      }
+  }
+  }
   },
   plugins: [],
 }
